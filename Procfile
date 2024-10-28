2ce5862313e70220dbb533b270bf59e73762e276
charan1911 <gummadicharan37@gmail.com>
2024-10-28 12:46:41 +0530

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn todolistproject.wsgi:application --log-file -
--- todolistproject/settings.py
+++ todolistproject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,5 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+django_heroku.settings(locals())
 
 LOGIN_URL = 'login-page'
